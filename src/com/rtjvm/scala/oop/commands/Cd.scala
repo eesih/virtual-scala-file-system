df80741d71a5e7ceb91f62fd7b28e85049285517
Eero Sihvonen <ext-eero.sihvonen@opr-vakuus.fi>
2019-04-02 11:37:48 +0300

    Cd relative paths

--- src/com/rtjvm/scala/oop/commands/Cd.scala
+++ src/com/rtjvm/scala/oop/commands/Cd.scala
@@ -17,18 +17,17 @@ class Cd(dir: String) extends Command {
       else if (wd.isRoot) wd.path + dir
       else wd.path + Directory.SEPARATOR + dir
 
-      val destinationDir = doFindEntry(root, absolutePath)
+    val destinationDir = doFindEntry(root, absolutePath)
 
-      if(destinationDir == null || !destinationDir.isDirectory)
-          state.setMessage(s"$dir: no such directory!")
-      else
-        State(root, destinationDir.asDirectory)
+    if (destinationDir == null || !destinationDir.isDirectory)
+      state.setMessage(s"$dir: no such directory!")
+    else
+      State(root, destinationDir.asDirectory)
 
 
   }
 
-
-  def doFindEntry(root: Directory, path: String):DirEntry = {
+  def doFindEntry(root: Directory, path: String): DirEntry = {
 
     @tailrec
     def findEntryHelper(curDir: Directory, path: List[String]): DirEntry =
@@ -40,9 +39,22 @@ class Cd(dir: String) extends Command {
         else findEntryHelper(nextDir.asDirectory, path.tail)
       }
 
-      val tokens: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
+    @tailrec
+    def collapseRelativeTokens(path: List[String], result: List[String]): List[String] = {
+      if (path.isEmpty) result
+      else if (".".equals(path.head)) collapseRelativeTokens(path.tail, result)
+      else if ("..".equals(path.head)) {
+        if (result.isEmpty) null
+        else collapseRelativeTokens(path.tail, result.init)
+      } else collapseRelativeTokens(path.tail, result :+ path.head)
+    }
 
-      findEntryHelper(root, tokens)
+    val tokens: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
+
+    val newTokens = collapseRelativeTokens(tokens, List())
+
+    if (newTokens == null) null
+    else findEntryHelper(root, newTokens)
   }
 
 }
